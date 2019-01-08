ae2bec2fe8c775c09c255fb32f0df53d1db83871
血衫非弧 <admin@kelu.org>
2019-01-08 13:12:22 +0800

    [update] docker-compose

--- keluLinuxKit.sh
+++ keluLinuxKit.sh
@@ -435,6 +435,10 @@ install_docker(){
     usermod -aG docker $USER
     systemctl enable docker
     systemctl start docker
+    curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    docker-compose --version
 #     update-rc.d docker enable
 #     update-rc.d docker start
 }
