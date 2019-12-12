769c116620333042ec2c9ce36416fe78df30766e
血衫非弧 <admin@kelu.org>
2019-12-12 21:24:48 +0800

    [update] aliyundun

--- keluLinuxKit.sh
+++ keluLinuxKit.sh
@@ -532,6 +532,33 @@ install_keluwechat(){
     run_docker_ss
 }
 
+install_yundun(){
+    cd $DOWNLOAD
+    wget http://update.aegis.aliyun.com/download/uninstall.sh
+    chmod +x uninstall.sh
+    ./uninstall.sh
+
+    wget http://update.aegis.aliyun.com/download/quartz_uninstall.sh
+    chmod +x quartz_uninstall.sh
+    ./quartz_uninstall.sh
+
+    pkill aliyun-service
+    rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
+    rm -rf /usr/local/aegis*
+
+    iptables -I INPUT -s 140.205.201.0/28 -j DROP
+    iptables -I INPUT -s 140.205.201.16/29 -j DROP
+    iptables -I INPUT -s 140.205.201.32/28 -j DROP
+    iptables -I INPUT -s 140.205.225.192/29 -j DROP
+    iptables -I INPUT -s 140.205.225.200/30 -j DROP
+    iptables -I INPUT -s 140.205.225.184/29 -j DROP
+    iptables -I INPUT -s 140.205.225.183/32 -j DROP
+    iptables -I INPUT -s 140.205.225.206/32 -j DROP
+    iptables -I INPUT -s 140.205.225.205/32 -j DROP
+    iptables -I INPUT -s 140.205.225.195/32 -j DROP
+    iptables -I INPUT -s 140.205.225.204/32 -j DROP
+}
+
 install_bbr(){
     cd $DOWNLOAD
     wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10/linux-image-4.10.0-041000-generic_4.10.0-041000.201702191831_amd64.deb
